f21de0da35ecf17d30966c4ad6f5fa0fe10d2f84
dukryung <dukryung@gmail.com>
2022-07-14 16:08:30 +0900

    fix: import path

--- server/auth/rest/handler.go
+++ server/auth/rest/handler.go
@@ -2,7 +2,7 @@ package rest
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/microservice/server/auth/rest/endpoint"
+	"github.com/dukryung/microservice/server/auth/rest/endpoint"
 )
 
 type Handler struct {
